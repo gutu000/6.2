052877ae172e62fd989bcc52c73627aa9e3e0c83
Elev <Elev@TEB125112>
2023-03-03 15:00:20 +0100

    Add project files.

---
+++ 6.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace hhej
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine(Störst(5, 16));
+        }
+      
+
+        static int Störst(int tal1, int tal2)
+        {
+            int Strst;
+            if (tal1 > tal2)
+            {
+                Strst = tal1;
+            }
+            else
+            {
+                Strst = tal2;
+            }
+            {
+                return Strst;
+            }
+        }
+    }
+}
